8f08b0ba50c4c2676ce5cb7944bc3fec52f79fd0
jishnu <jishnuks9421@gmail.com>
2024-03-12 18:09:22 +0530

    initial homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import "../app.css";
+	import Footer from "./footer.svelte";
+	import Navbar from "./navbar.svelte";
   </script>
-  
+  <Navbar />
   <slot />
+  <Footer />
---
+++ src/routes/landing/content.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import photographer from '$lib/images/photographer.png';
+   let ph= photographer;
+</script>
+<div class="lg:flex">
+    <div class="lg:grid lg:w-1/2">
+        <img src={ph} alt="" class="w-full">
+    </div>
+    <div class="lg:w-1/2 flex flex-col justify-center items-center">
+        <h1 class="text-3xl font-bold mb-3">For Events</h1>
+        <div class=" container flex flex-wrap">
+            <div class="lg:w-1/2 md:w-1/2">
+                <div class="lg:w-1/2 md:w-1/2">
+                    <h1 class="font-bold">Instant photo sharing</h1>
+                    <p>Instant availability of photos increases photo sharing on social media.</p>
+                </div>
+            </div>
+            <div class=" lg:w-1/2 md:w-1/2">
+                <div class="lg:w-1/2 md:w-1/2">
+                <h1 class="font-bold">Swift Event Creation</h1>
+                <p>Create a dedicated themed events’ page with schedule and sessions.</p>
+                </div>
+            </div>
+            <div class=" lg:w-1/2 md:w-1/2">
+                <div class="lg:w-1/2 md:w-1/2">
+                <h1 class="font-bold">Happy Sponsors</h1>
+                <p>Effortlessly push sponsor logo and content to all attendees. Use sponsor insights to measure the efficacy.</p>
+                </div>
+            </div>
+            <div class=" lg:w-1/2 md:w-1/2">
+                <div class="lg:w-1/2 md:w-1/2">
+                <h1 class="font-bold">All in one place</h1>
+                <p>It is an event marketing tool, its your DAM, its a CRM, its an expense tracker. Its all in one.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
